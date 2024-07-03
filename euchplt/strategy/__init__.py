27715b555ece3e88d85686ea2fa7377ada6ee9ff
crash <ltung783@gmail.com>
2024-07-03 14:39:50 -0500

    import functions from __main__.py for sphinx

--- euchplt/strategy/__init__.py
+++ euchplt/strategy/__init__.py
@@ -5,3 +5,5 @@ from .random import StrategyRandom
 from .simple import StrategySimple
 from .smart import StrategySmart
 from .ml import StrategyML
+# for sphinx
+from .__main__ import tune_strategy_smart, main
