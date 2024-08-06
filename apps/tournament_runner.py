3837ee356a5ec9c45219546a9e4900e316830145
crash <ltung783@gmail.com>
2024-08-05 21:00:00 -0700

    add charting stuff to dashboard (chart.js is super-easy to use!)

--- apps/tournament_runner.py
+++ apps/tournament_runner.py
@@ -30,7 +30,7 @@ import shelve
 from flask import Flask, session, request, render_template, abort
 
 from euchplt.core import cfg
-from euchplt.tournament import Tournament, LB_PRINT_STATS
+from euchplt.tournament import Tournament, LBStat, LB_PRINT_STATS
 
 #########
 # Setup #
@@ -85,23 +85,23 @@ def get_db_file(tourn_id: str) -> str:
     """
     return f"tourn-{tourn_id}.db"
 
-def save_tournament(tourn_id: str, tourn: Tournament) -> None:
+def save_tourn_info(tourn_id: str, info: dict) -> None:
     """Persist the tournament information (including stats)
     """
     db_file = get_db_file(tourn_id)
     db_path = os.path.join(RESOURCES_DIR, db_file)
     with shelve.open(db_path, flag='c') as db:
-        db['tourn'] = tourn
+        db.update(info)
 
-def retrieve_tournament(tourn_id: str) -> Tournament:
+def retrieve_tourn_info(tourn_id: str) -> dict:
     """Retrieve the tournament information (including stats)
     """
     db_file = get_db_file(tourn_id)
     db_path = os.path.join(RESOURCES_DIR, db_file)
     with shelve.open(db_path) as db:
         # ATTN: can probably do this without the comprehension loop!!!
-        data = {k: v for k, v in db.items()}
-    return data.get('tourn')
+        info = {k: v for k, v in db.items()}
+    return info
 
 FLOAT_PREC = 1
 
@@ -124,6 +124,13 @@ SUBMIT_FUNCS = [
 
 INIT_TIMER = "0:00"
 INIT_START = 0  # to make parseInt() work
+
+POS_STAT = "Position"
+
+CHART_LB_STATS = [
+    LBStat.WIN_PCT,
+    LBStat.CUR_ELO
+]
 
 @app.get("/")
 def index():
@@ -170,8 +177,17 @@ def run_tourn(form: dict) -> str:
     # TODO: get override parameters from form, used to instantiate tournament!!!
     tourn = Tournament.new(tourn_name)
     tourn_id = gen_tourn_id(tourn)
-    save_tournament(tourn_id, tourn)
     session['tourn_id'] = tourn_id
+
+    ch_data = {}
+    ch_data['teams'] = list(tourn.teams)  # team names
+    ch_data['stats'] = {}
+    for stat in [POS_STAT] + [str(x) for x in CHART_LB_STATS]:
+        ch_data['stats'][stat] = {}
+        for team in ch_data['teams']:
+            ch_data['stats'][stat][team] = []
+
+    save_tourn_info(tourn_id, {'tourn': tourn, 'ch_data': ch_data})
 
     run_msg = "Starting tournament..."
     context = {
@@ -180,6 +196,8 @@ def run_tourn(form: dict) -> str:
         'lb_td_cls':  None,
         'lb_header':  None,
         'lb_data':    None,
+        'ch_labels':  None,
+        'ch_data':    None,
         'timer':      INIT_TIMER,
         'start':      INIT_START,
         'winner':     None,
@@ -204,20 +222,22 @@ def next_pass(form: dict) -> str:
     """
     winner = None
     tourn_id = session['tourn_id']
-    tourn = retrieve_tournament(tourn_id)
+    info = retrieve_tourn_info(tourn_id)
+    tourn = info.get('tourn')
+    ch_data = info.get('ch_data')
+    assert tourn and ch_data
     # REVISIT: clunky use of local `pass_num`, see note in `RoundRobin.run_pass()`!!!
     pass_num = tourn.pass_num + 1
     assert pass_num < tourn.passes
 
     lb = tourn.run_pass(pass_num)
-    save_tournament(tourn_id, tourn)
     if pass_num + 1 < tourn.passes:
         assert pass_num + 1 == len(tourn.leaderboards)
         pass_msg = f"Completed pass {pass_num + 1} of {tourn.passes}"
     else:
         plural = "s" if len(tourn.winner) > 1 else ""
         winner = ', '.join(tourn.winner)
-        pass_msg = f"Tournament \"{tourn.name}\" complete, winner{plural}: {winner}"
+        pass_msg = f"Tournament complete, winner{plural}: {winner}"
 
     stats_row = next(iter(lb.values()))
     # note that all of the following include team info in the [0] position
@@ -227,11 +247,16 @@ def next_pass(form: dict) -> str:
                                 if k in LB_PRINT_STATS]
     lb_header  = ["Team"]    + [str(k) for k in stats_row.keys()
                                 if k in LB_PRINT_STATS]
-    lb_data   = []
-    for team, stats in lb.items():
+    lb_data = []
+    for idx, (team, stats) in enumerate(lb.items()):
         row = [team] + [round_val(v) for k, v in stats.items()
                         if k in LB_PRINT_STATS]
         lb_data.append(row)
+        ch_data['stats'][POS_STAT][team].append(-idx)
+        for stat in CHART_LB_STATS:
+            ch_data['stats'][str(stat)][team].append(round_val(stats[stat]))
+
+    save_tourn_info(tourn_id, {'tourn': tourn, 'ch_data': ch_data})
 
     # this won't be perfectly correlated with client side timer, so sometimes
     # glitchy, but as close as we can get
@@ -244,6 +269,8 @@ def next_pass(form: dict) -> str:
         'lb_td_cls':  lb_td_cls,
         'lb_header':  lb_header,
         'lb_data':    lb_data,
+        'ch_labels':  list(range(1, tourn.passes + 1)),
+        'ch_data':    ch_data,
         'timer':      timer,
         'start':      form.get('start'),
         'winner':     winner,
